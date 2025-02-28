06cf41441a7ac47f65ae3425832a344c92e273e4
vicct0r <v1ctor_inacio@proton.me>
2025-02-28 06:17:47 -0300

    quick fix - no migrations

---
+++ backend/usuarios/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['first_name', 'last_name', 'email', 'telefone', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações Pessoais', {'fields': ('first_name', 'last_name', 'telefone')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')})
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
+    )
