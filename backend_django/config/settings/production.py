7a9f9f43c25f84b1bb8f8f8e0d34c46032573174
Victor Inácio <165459150+vicct0r@users.noreply.github.com>
2025-02-28 06:11:59 -0300

    Delete backend_django directory

--- backend_django/config/settings/production.py
+++
@@ -1,8 +0,0 @@
-from .base import *
-
-SECRET_KEY = 'django-insecure-tn6jo#x5*68l!smd6*k2*37o-hbj6n3hw&jwnor)of^iua^ygv'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
